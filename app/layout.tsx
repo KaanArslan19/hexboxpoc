eb744d2595d77b0fcbb5b968fa91d3b7c8587187
KaanArslan19 <kaanarslan35@gmail.com>
2024-12-01 22:40:14 +1000

    scripts added for google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import AuthSession from "./components/AuthSession";
 import Wallet from "./components/Wallet";
 import { getServerSession } from "next-auth/next";
+import Script from "next/script";
 //import { authOptions } from "./utils/auth";
 
 export const metadata: Metadata = {
@@ -22,7 +23,45 @@ export default async function RootLayout({
     <Wallet>
       <AuthSession>
         <html lang="en">
-          <body>{children}</body>
+          <head>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-PWL3ZE897B"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PWL3ZE897B');
+          `}
+            </Script>
+
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WGRTDFGZ');
+          `}
+            </Script>
+          </head>
+          <body>
+            <noscript>
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-WGRTDFGZ"
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              ></iframe>
+            </noscript>
+            {children}
+          </body>
         </html>
       </AuthSession>
     </Wallet>
