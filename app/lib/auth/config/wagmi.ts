5a25b4e8172583224d430daafd96bc476676810a
Bradley Heubel <bradley.heubel@gmail.com>
2025-02-03 20:21:10 +1100

    HEXBOX-118 - Wallet connection changes
    - Added support for Avalanche Fuji
    - Commented out all other networks since we don't need them right now.

--- app/lib/auth/config/wagmi.ts
+++ app/lib/auth/config/wagmi.ts
@@ -5,31 +5,34 @@ import {
   optimismSepolia,
   arbitrumSepolia,
   zkSyncSepoliaTestnet,
-  polygonMumbai
+  polygonMumbai,
+  avalancheFuji,
 } from 'viem/chains';
 import { cookieStorage, createConfig, createStorage, http } from 'wagmi';
 
 export default createConfig({
   chains: [
-    sepolia,
-    baseSepolia,
-    scrollSepolia,
-    optimismSepolia,
-    arbitrumSepolia,
-    zkSyncSepoliaTestnet,
-    polygonMumbai
+    avalancheFuji,
+    // sepolia,
+    // baseSepolia,
+    // scrollSepolia,
+    // optimismSepolia,
+    // arbitrumSepolia,
+    // zkSyncSepoliaTestnet,
+    // polygonMumbai
   ],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage
   }),
   transports: {
-    [sepolia.id]: http(),
-    [baseSepolia.id]: http(),
-    [scrollSepolia.id]: http(),
-    [optimismSepolia.id]: http(),
-    [arbitrumSepolia.id]: http(),
-    [zkSyncSepoliaTestnet.id]: http(),
-    [polygonMumbai.id]: http()
+    [avalancheFuji.id]: http(),
+    // [sepolia.id]: http(),
+    // [baseSepolia.id]: http(),
+    // [scrollSepolia.id]: http(),
+    // [optimismSepolia.id]: http(),
+    // [arbitrumSepolia.id]: http(),
+    // [zkSyncSepoliaTestnet.id]: http(),
+    // [polygonMumbai.id]: http()
   }
 });
