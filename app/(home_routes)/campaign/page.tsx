c403f528a200a0248cd99fdf507620f208c07b45
KaanArslan19 <kaanarslan35@gmail.com>
2025-05-25 14:59:17 +1000

    HEXBOX-285 share button second iteration

--- app/(home_routes)/campaign/page.tsx
+++ app/(home_routes)/campaign/page.tsx
@@ -1,20 +1,61 @@
 import CampaignDetails from "@/app/components/campaign/CampaignDetails";
 import { fetchSingleCampaign } from "@/app/utils/apiHelpers";
-import React from "react";
 import { getProducts } from "@/app/utils/poc_utils/getProducts";
-import Head from "next/head";
+import { Metadata, ResolvingMetadata } from "next";
+
 interface Props {
   searchParams: { campaignId: string };
 }
 
+export async function generateMetadata(
+  { searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const campaignId = searchParams.campaignId;
+  const campaign = await fetchSingleCampaign(campaignId);
+
+  if (!campaign) {
+    return {
+      title: "Campaign not found",
+    };
+  }
+
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/campaign?campaignId=${campaignId}`;
+
+  return {
+    title: campaign.title,
+    description: campaign.description,
+    openGraph: {
+      title: campaign.title,
+      description: campaign.description,
+      url: url,
+      images: [
+        {
+          url: campaign.imageUrl,
+          width: 1200,
+          height: 630,
+          alt: campaign.title,
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: campaign.title,
+      description: campaign.description,
+      images: [campaign.imageUrl],
+    },
+  };
+}
+
 export default async function CampaignPage({ searchParams }: Props) {
   const campaignId = searchParams.campaignId;
-
   const campaign = await fetchSingleCampaign(campaignId);
+
   if (!campaign) {
     return <div>Campaign not found</div>;
   }
-  console.log(campaign, "campaignDetailsPAgesss");
+
   const plainCampaign = {
     ...campaign,
     _id: campaign._id.toString(),
@@ -22,35 +63,8 @@ export default async function CampaignPage({ searchParams }: Props) {
   };
   const products = await getProducts(campaignId);
 
-  console.log(products, "products");
-
   return (
     <div>
-      <Head>
-        <title>{campaign.title}</title>
-        <meta name="title" content={campaign.title} />
-        <meta name="description" content={campaign.description} />
-
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content={`https://poc.hexbox.money/campaign/${campaign.id}`}
-        />
-        <meta property="og:title" content={campaign.title} />
-        <meta property="og:description" content={campaign.description} />
-        <meta property="og:image" content={campaign.imageUrl} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:url"
-          content={`https://poc.hexbox.money/campaign/${campaign.id}`}
-        />
-        <meta name="twitter:title" content={campaign.title} />
-        <meta name="twitter:description" content={campaign.description} />
-        <meta name="twitter:image" content={campaign.imageUrl} />
-      </Head>
       <CampaignDetails {...plainCampaign} products={products} />
     </div>
   );
